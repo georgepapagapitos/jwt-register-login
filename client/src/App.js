ff7b7a1eb8fa0b398537d7382bb51fbcb13f1bcd
George Papagapitos <george.g.papagapitos@gmail.com>
2021-09-16 16:23:34 -0500

    set up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
-    <>
-    </>
+    <Router>
+      <div className='container'>
+        <Switch>
+          <Route exact path='/login' render={props => <Login {...props} />} />
+          <Route eact path='/register' render={props => <Register {...props} />} />
+          <Route exact path='/dashboard' render={props => <Dashboard {...props} />} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

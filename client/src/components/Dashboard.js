0ba06e16195d371899a960ceb6cc5254e2172a57
George Papagapitos <george.g.papagapitos@gmail.com>
2021-09-17 11:48:26 -0500

    dashboard working

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,44 @@
-const Dashboard = ({setAuth}) => {
+import { useState, useEffect } from 'react';
+
+const Dashboard = ({ setAuth }) => {
+
+  const [user, setUser] = useState({
+    username: '',
+    email: ''
+  });
+
+  useEffect(() => {
+    const getUsername = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/dashboard/', {
+          method: 'GET',
+          headers: { token: localStorage.token }
+        });
+
+        const parseResponse = await response.json();
+
+        setUser({...user, username: parseResponse.username, email: parseResponse.email});
+
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+
+    getUsername();
+  });
+
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem('token');
+    setAuth(false);
+  }
+
   return (
     <>
       <h1>Dashboard</h1>
-      <button onClick={() => setAuth(false)}>Logout</button>
+      <p>Hello, {user.username}</p>
+      <p>Your registered email is {user.email}</p>
+      <button onClick={e => logout(e)} className='btn btn-primary'>Logout</button>
     </>
   );
 }
